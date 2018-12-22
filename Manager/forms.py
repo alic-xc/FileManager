449577a4be5b1718abf61053e9e2bc9cf1a08950
alic-xc <aladelekan187@gmail.com>
2018-12-22 23:49:07 +0100

    working on the form for input attributes. Though flask is simpler than django

--- Manager/forms.py
+++ Manager/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class folderForm(forms.Form):
-    folder_name = forms.CharField(label='Folder Name', max_length=50)
-    hidden = forms.BooleanField(label='Hidden')
+    folder_name = forms.CharField(label='Folder Name', max_length=50,
+                                  widget= forms.TextInput(attrs={'placeholder':'Type Folder Name Here','class':'form-control'}) )
+    status = forms.ChoiceField(label='Status', choices=((True, 'Hidden'),(False,'Unhide')))
 
 class videoForm():
     pass
