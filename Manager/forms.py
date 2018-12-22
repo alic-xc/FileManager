04999883a60ec61b06b82ef8172a354468710198
alic-xc <aladelekan187@gmail.com>
2018-12-23 00:05:58 +0100

    working on the folder creation on django fs

--- Manager/forms.py
+++ Manager/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 class folderForm(forms.Form):
     folder_name = forms.CharField(label='Folder Name', max_length=50,
-                                  widget= forms.TextInput(attrs={'placeholder':'Type Folder Name Here','class':'form-control'}) )
-    status = forms.ChoiceField(label='Status', choices=((True, 'Hidden'),(False,'Unhide')))
+                                  widget= forms.TextInput(attrs={'placeholder':'Type Folder Name Here','class':'form-control input-sm'}) )
+    status = forms.ChoiceField(label='Status', choices=(('','-- select folder attribute --'),(True, 'Hidden'),(False,'Unhide')),
+                               widget= forms.Select(attrs={'class':'form-control input-sm'}),
+                               )
 
 class videoForm():
     pass
