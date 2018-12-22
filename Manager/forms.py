80a4618cc9559f958f3ec99e792c0c301db3ac07
alic-xc <aladelekan187@gmail.com>
2018-12-22 22:48:11 +0100

    rectifying label mispelling and also updating my folder template

--- Manager/forms.py
+++ Manager/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class folderForm(forms.Form):
     folder_name = forms.CharField(label='Folder Name', max_length=50)
-    hidden = forms.BooleanField(lable='Hidden', bool=False)
+    hidden = forms.BooleanField(label='Hidden')
 
 class videoForm():
     pass
