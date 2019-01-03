fd9202c04515c2c1f9826443638d5fc81822d63d
alic-xc <aladelekan187@gmail.com>
2019-01-03 17:46:15 +0100

    working and perfecting form in django and working on the audio form so that javascript can display all available information

--- Manager/forms.py
+++ Manager/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 class folderForm(forms.Form):
     folder_name = forms.CharField(label='Folder Name', max_length=50,
-                                  widget= forms.TextInput(attrs={'placeholder':'Type Folder Name Here','class':'form-control input-sm'}) )
-    status = forms.ChoiceField(label='Status', choices=(('','-- select folder attribute --'),(True, 'Hidden'),(False,'Unhide')),
+                                  widget= forms.TextInput(
+                                      attrs={'placeholder':'Type Folder Name Here','class':'form-control input-sm'}) )
+    status = forms.ChoiceField(label='Status',
+                               choices=(('','-- select folder attribute --'),(True, 'Hidden'),(False,'Unhide')),
                                widget= forms.Select(attrs={'class':'form-control input-sm'}),
                                )
 
@@ -16,7 +18,19 @@ class pictureForm():
 class documentForm():
     pass
 
-class audioForm():
-    pass
+class audioForm(forms.Form):
+
+    name = forms.CharField(label='Audio Name', max_length=50,
+                                 widget=forms.TextInput(
+                                     attrs={'placeholder':'Enter Name Here','class':'form-control input-sm'}))
+    format = forms.ChoiceField(label='Audio Format',
+                             choices=(('','--select Audio format--'),('MP3','MP3'),('WMA','WMA'),('WAV'),('WAV')),
+                             widget=forms.TextInput(
+                                 attrs={'class':'form-control input-sm'}
+                             ))
+    summary = forms.CharField(label='Audio Summary', max_length=200,
+                              widget=forms.TextInput(
+                                  attrs={'placeholder':'Enter Audio Summary','class':'form-control input-sm'}
+                              ))
 
 
