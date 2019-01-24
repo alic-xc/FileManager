054bc6a853aefe5b88f0a85e9f542649e13830e6
alic-xc <aladelekan187@gmail.com>
2019-01-24 14:36:49 +0100

    still working on test file

--- Manager/tests.py
+++ Manager/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
+from .forms import *
 
 # Create your tests here.
+
+
+class FileManagerTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def testing_folder(self):
+        c = Client()
+        form = FolderForm(data = {'folder_name':'secret','status':True})
+
+        response = c.post('/folder', form)
+        self.assertEqual(response.status_code , 200)
